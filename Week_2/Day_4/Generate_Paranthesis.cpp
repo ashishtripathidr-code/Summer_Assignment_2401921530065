f32b90526f62ec56dc10b31ef4e61a318bd52246
ashishtripathidr-code <ashishtripathidr@gmail.com>
2026-06-11 13:50:30 +0530

    Implement function to generate parentheses combinations

---
+++ Week_2/Day_4/Generate_Paranthesis.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+      void Generate(int n,int left,int right,vector<string>&ans,string &temp){
+            if(left+right==2*n){
+                ans.push_back(temp);
+                return;
+            }
+            if(left<n){
+                temp.push_back('(');
+                Generate(n,left+1,right,ans,temp);
+                temp.pop_back();
+            }
+            if(right<left){
+                temp.push_back(')');
+                Generate(n,left,right+1,ans,temp);
+                temp.pop_back();
+            }
+        }
+    vector<string> generateParenthesis(int n) {
+        string temp;
+        vector<string>ans;
+        Generate(n,0,0,ans,temp);
+       
+        return ans;
+        
+    }
+};
