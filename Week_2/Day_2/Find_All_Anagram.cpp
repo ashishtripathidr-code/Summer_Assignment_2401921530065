4c9969476f84e8eb746bb4137170f1633be705a0
ashishtripathidr-code <ashishtripathidr@gmail.com>
2026-06-14 09:46:12 +0530

    Refactor anagram finding logic and add helper function
    Added a helper function to check if all elements in the count vector are zero. Updated the logic to find anagrams by using a single count vector instead of separate vectors for s and p.

--- Week_2/Day_2/Find_All_Anagram.cpp
+++ Week_2/Day_2/Find_All_Anagram.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
+     bool allzero(vector<int>count){
+        for(int i=0;i<count.size();i++){
+            if(count[i]!=0)
+            return 0;
+        }
+        return 1;
+     }
     vector<int> findAnagrams(string s, string p) {
-             vector<int> result;
-        if (s.size() < p.size()) return result;
-
-        vector<int> countP(26, 0), countS(26, 0);
+        vector<int> result;
+        vector<int>count(26, 0);
 
         for (int i=0;i<p.size();i++) {
-            countP[p[i] - 'a']++;
+            count[p[i] - 'a']++;
         }
-        for (int i = 0; i < s.size(); i++) {
-            countS[s[i] - 'a']++;
-
-            if (i >= p.size()) {
-                countS[s[i - p.size()] - 'a']--;
-            }
-
-            if (countS == countP) {
-                result.push_back(i - p.size() + 1);
-            }
-        }
+         int i=0,j=0;
+         int k=p.size();
+         while(j<s.size()){
+            count[s[j]-'a']--;
+             if(j-i+1==k){
+                if(allzero(count)){
+                    result.push_back(i);
+                }
+                count[s[i]-'a']++;
+                i++;
+             }
+             j++;
+         }
 
         return result;
     }
