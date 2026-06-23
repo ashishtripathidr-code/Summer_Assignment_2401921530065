2d0890b1e831edb53b98a64a82beb6f8b1499ebc
ashishtripathidr-code <ashishtripathidr@gmail.com>
2026-06-23 20:37:05 +0530

    Implement binary tree inversion function

---
+++ Week_4/Day_1/Invert_Binary_Tree.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(!root)
+        return 0;
+        TreeNode*temp=root->right;
+        root->right=root->left;
+        root->left=temp;
+        invertTree(root->left);
+        invertTree(root->right);
+        return root;
+    }
+};
